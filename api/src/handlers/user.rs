6cb1b9d69f5b0c9b61a722a30a19b7c1ca7d75b8
srosati <srosati@itba.edu.ar>
2023-05-17 22:42:04 -0300

    feat: public_key is document id

--- api/src/handlers/user.rs
+++ api/src/handlers/user.rs
@@ -7,14 +7,14 @@ use mongodb::{Client, Collection};
 use mongodb::error::ErrorKind;
 use serde::Serialize;
 use crate::handlers::common::HandlerResponse;
-use crate::model::user::User;
+use crate::model::user::{CreateUser, User};
 
-pub async fn create_user(State(state): State<Client>, Json(payload): Json<User>) -> HandlerResponse {
+pub async fn create_user(State(state): State<Client>, Json(payload): Json<CreateUser>) -> HandlerResponse {
     println!("Create user: {}", payload.public_key);
     let db = state.database("passphrasex");
-    let collection: Collection<User> = db.collection("users");
+    let collection: Collection<CreateUser> = db.collection("users");
     match collection.insert_one(&payload, None).await {
-        Ok(_) => HandlerResponse::new(StatusCode::CREATED, payload),
+        Ok(_) => HandlerResponse::new(StatusCode::CREATED, User::from_pk(payload.public_key)),
         Err(err) =>
             HandlerResponse::new(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
     }
--- api/src/model/user.rs
+++ api/src/model/user.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct User {
+pub struct CreateUser {
+    #[serde(rename(serialize = "_id"))]
     pub public_key: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    #[serde(rename(serialize = "public_key"))]
+    pub _id: String,
+    pub passwords: HashMap<String, HashMap<String, String>>
+}
+
+impl User {
+    pub fn from_pk(public_key: String) -> Self {
+        Self {
+            _id: public_key,
+            passwords: HashMap::new()
+        }
+    }
 }
