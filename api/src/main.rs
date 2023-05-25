4ac2054b92af4275407b0fb89781f42416f2e4c4
srosati <srosati@itba.edu.ar>
2023-05-25 16:43:26 -0300

    refactor: user & password controller

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,13 +7,9 @@ use model::common::DatabaseConfig;
 use service::user::UserService;
 use service::password::PasswordService;
 
-use handlers::middleware::auth::only_user;
-use handlers::user::create_user;
-use handlers::password::add_password;
+use handlers::user::UserController;
 
-use axum::{routing::{post}, Router, middleware};
-use axum::routing::get;
-use crate::handlers::password::list_passwords;
+use axum::Router;
 
 #[derive(Clone)]
 pub struct AppData {
@@ -23,28 +19,19 @@ pub struct AppData {
 
 #[tokio::main]
 async fn main() {
-    let no_middleware_router = Router::new()
-        .route("/users", post(create_user));
+    let user_controller = UserController::new();
 
-    let auth_router = Router::new()
-        .route("/users/:user_id/passwords", post(add_password))
-        .route("/users/:user_id/passwords", get(list_passwords))
-        .route_layer(middleware::from_fn(only_user));
-    
-    let db_client = DatabaseConfig::new()
+    let client = DatabaseConfig::new()
         .into_client()
         .await
         .expect("Failed to connect to database");
 
     let app = Router::new()
-        .merge(no_middleware_router)
-        .merge(auth_router)
+        .merge(user_controller.router)
         .with_state(AppData {
-        user_service: UserService::new(&db_client),
-        password_service: PasswordService::new(&db_client)
+        user_service: UserService::new(&client),
+        password_service: PasswordService::new(&client)
     });
-
-
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().expect("Failed to parse address"))
