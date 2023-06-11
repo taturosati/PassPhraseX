21581218357bcdc3ee7f1a14b5b4cc32d2bda392
Santos Rosati <65305492+srosati@users.noreply.github.com>
2023-06-11 14:15:06 -0300

    feat: edit / delete password (#18)
    * feat: api hello world
    
    * feat: create user outline
    
    * refactor: better error handling on create_user
    
    * feat: public_key is document id
    
    * feat: add password
    
    * refactor: handler response & add password
    
    * refactor: services
    
    * refactor: services
    
    * feat: list passwords
    
    * feat: auth middleware
    
    * refactor: save username & password separately
    
    * refactor: user & password controller
    
    * merge: main_api
    
    * feat: save user to api
    
    * feat: save password on api
    
    * feat: get passwords for site
    
    * refactor: extract file functionality to file
    
    * refactor: better error handling
    
    * fix: merge
    
    * refactor: model to common crate (#8)
    
    * feat: login
    
    * refactor: move crypto functionality to folder
    
    * feat: gen, store & verify device pass hash
    
    * feat: encrypt private key on disk
    
    * feat: decrypt pk with device pass
    
    * feat: auth on api (#10)
    
    * feat: auth on cli
    
    * feat: dont allow duplicate site+username
    
    * feat: add _id field to passwords (#15)
    
    * feat: add _id field to passwords
    
    * feat: edit password
    
    * refactor: use password_id for better access
    
    * feat: delete password
    
    * feat: edit/delete password (#17)

--- api/src/handlers/password.rs
+++ api/src/handlers/password.rs
@@ -1,21 +1,23 @@
 use axum::{extract::{Path, State}, http::StatusCode, Json, middleware, Router};
 use axum::body::Body;
-use axum::routing::{get, post};
+use axum::routing::{delete, get, post, put};
 use crate::AppData;
 use crate::handlers::common::HandlerResponse;
 use crate::handlers::middleware::auth::only_user;
 use common::model::password::Password;
 
-
 pub struct PasswordController {
     pub router: Router<AppData, Body>
 }
+
 
 impl PasswordController {
     pub fn new() -> Self {
         let router = Router::new()
             .route("/users/:user_id/passwords", post(Self::add_password))
             .route("/users/:user_id/passwords", get(Self::list_passwords))
+            .route("/users/:user_id/passwords/:password_id", delete(Self::delete_password))
+            .route("/users/:user_id/passwords/:password_id/password", put(Self::modify_password))
             .route_layer(middleware::from_fn(only_user));
 
         Self {
@@ -23,7 +25,11 @@ impl PasswordController {
         }
     }
 
-    pub async fn add_password(State(state): State<AppData>, Path(user_id): Path<String>, Json(payload): Json<Password>) -> HandlerResponse {
+    pub async fn add_password(
+        State(state): State<AppData>,
+        Path(user_id): Path<String>,
+        Json(payload): Json<Password>
+    ) -> HandlerResponse {
         // TODO: Multiple error types
         match state.password_service.add_password(user_id, payload).await {
             Ok(password) => HandlerResponse::new(StatusCode::CREATED, password),
@@ -31,10 +37,35 @@ impl PasswordController {
         }
     }
 
-    pub async fn list_passwords(State(state): State<AppData>, Path(user_id): Path<String>) -> HandlerResponse {
+    pub async fn list_passwords(
+        State(state): State<AppData>,
+        Path(user_id): Path<String>
+    ) -> HandlerResponse {
         // TODO: Multiple error types
         match state.password_service.list_passwords(user_id).await {
             Ok(passwords) => HandlerResponse::new(StatusCode::OK, passwords),
+            Err(err) => HandlerResponse::new(StatusCode::INTERNAL_SERVER_ERROR, err)
+        }
+    }
+
+    pub async fn delete_password(
+        State(state): State<AppData>,
+        Path((user_id, password_id)): Path<(String, String)>
+    ) -> HandlerResponse {
+        // TODO: Multiple error types
+        match state.password_service.delete_password(user_id, password_id).await {
+            Ok(_) => HandlerResponse::new(StatusCode::NO_CONTENT, ()),
+            Err(err) => HandlerResponse::new(StatusCode::INTERNAL_SERVER_ERROR, err)
+        }
+    }
+
+    pub async fn modify_password(
+        State(state): State<AppData>,
+        Path((user_id, password_id)): Path<(String, String)>,
+        payload: String
+    ) -> HandlerResponse {
+        match state.password_service.modify_password(user_id, password_id, payload).await {
+            Ok(_) => HandlerResponse::new(StatusCode::NO_CONTENT, ()),
             Err(err) => HandlerResponse::new(StatusCode::INTERNAL_SERVER_ERROR, err)
         }
     }
