2e0bbf4be16820f7b869b590a1fb247fe11f9201
srosati <srosati@itba.edu.ar>
2023-06-25 13:56:37 -0300

    feat: better error handling

--- cli/src/file.rs
+++ cli/src/file.rs
@@ -1,67 +1,64 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::{Read, Write};
+use crate::{CredentialsMap, APP_INFO};
 use app_dirs2::{app_dir, AppDataType};
 use passphrasex_common::crypto::common::EncryptedValue;
 use passphrasex_common::crypto::symmetric::{decrypt_data, encrypt_data};
-use crate::{APP_INFO, CredentialsMap};
+use std::fs::File;
+use std::io::{Read, Write};
 
 const DATA_DIR: &str = "data";
 const PASSWORD_HASH_FILE: &str = "device_pass";
 const PRIVATE_KEY_FILE: &str = "private_key";
 const DATA_FILE: &str = "data.json";
 
-fn write_bytes(file_name: &str, bytes: Vec<u8>) -> Result<(), Box<dyn Error>> {
-	let path_to_file = app_dir(AppDataType::UserData, &APP_INFO, DATA_DIR)?
-		.join(file_name);
+fn write_bytes(file_name: &str, bytes: Vec<u8>) -> anyhow::Result<()> {
+    let path_to_file = app_dir(AppDataType::UserData, &APP_INFO, DATA_DIR)?.join(file_name);
 
-	let mut file = File::create(path_to_file)?;
-	file.write_all(&bytes)?;
+    let mut file = File::create(path_to_file)?;
+    file.write_all(&bytes)?;
 
-	Ok(())
+    Ok(())
 }
 
-fn read_bytes(file_name: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-	let path_to_file = app_dir(AppDataType::UserData, &APP_INFO, DATA_DIR)?
-		.join(file_name);
+fn read_bytes(file_name: &str) -> anyhow::Result<Vec<u8>> {
+    let path_to_file = app_dir(AppDataType::UserData, &APP_INFO, DATA_DIR)?.join(file_name);
 
-	let mut file = File::open(path_to_file)?;
-	let mut bytes = Vec::new();
-	file.read_to_end(&mut bytes)?;
+    let mut file = File::open(path_to_file)?;
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes)?;
 
-	Ok(bytes)
+    Ok(bytes)
 }
 
-pub fn write_password_hash(hash: &EncryptedValue) -> Result<(), Box<dyn Error>> {
-	write_bytes(PASSWORD_HASH_FILE, hash.to_string().as_bytes().to_vec())
+pub fn write_password_hash(hash: &EncryptedValue) -> anyhow::Result<()> {
+    write_bytes(PASSWORD_HASH_FILE, hash.to_string().as_bytes().to_vec())
 }
 
-pub fn read_password_hash() -> Result<EncryptedValue, Box<dyn Error>> {
-	let bytes = read_bytes(PASSWORD_HASH_FILE)?;
-	Ok(EncryptedValue::from(String::from_utf8(bytes)?))
+pub fn read_password_hash() -> anyhow::Result<EncryptedValue> {
+    let bytes = read_bytes(PASSWORD_HASH_FILE)?;
+    Ok(EncryptedValue::from(String::from_utf8(bytes)?))
 }
 
-pub fn write_sk(sk: &[u8;32], device_pass_hash: &str) -> Result<(), Box<dyn Error>> {
-	let enc = encrypt_data(device_pass_hash, sk)?;
-	write_bytes(PRIVATE_KEY_FILE, enc)
+pub fn write_sk(sk: &[u8; 32], device_pass_hash: &str) -> anyhow::Result<()> {
+    let enc = encrypt_data(device_pass_hash, sk)?;
+    write_bytes(PRIVATE_KEY_FILE, enc)
 }
 
-pub fn read_sk(device_pass_hash: &str) -> Result<[u8;32], Box<dyn Error>> {
-	let bytes = read_bytes(PRIVATE_KEY_FILE)?;
+pub fn read_sk(device_pass_hash: &str) -> anyhow::Result<[u8; 32]> {
+    let bytes = read_bytes(PRIVATE_KEY_FILE)?;
 
-	let dec = decrypt_data(device_pass_hash, bytes)?;
+    let dec = decrypt_data(device_pass_hash, bytes)?;
 
-	let mut content: [u8;32] = [0;32];
-	content.copy_from_slice(&dec[..32]);
-	Ok(content)
+    let mut content: [u8; 32] = [0; 32];
+    content.copy_from_slice(&dec[..32]);
+    Ok(content)
 }
 
-pub fn write_app_data(data: &CredentialsMap) -> Result<(), Box<dyn Error>> {
-	write_bytes(DATA_FILE, serde_json::to_string(&data)?.as_bytes().to_vec())
+pub fn write_app_data(data: &CredentialsMap) -> anyhow::Result<()> {
+    write_bytes(DATA_FILE, serde_json::to_string(&data)?.as_bytes().to_vec())
 }
 
-pub fn read_app_data() -> Result<CredentialsMap, Box<dyn Error>> {
-	let bytes = read_bytes(DATA_FILE)?;
-	let data: CredentialsMap = serde_json::from_slice(&bytes)?;
-	Ok(data)
+pub fn read_app_data() -> anyhow::Result<CredentialsMap> {
+    let bytes = read_bytes(DATA_FILE)?;
+    let data: CredentialsMap = serde_json::from_slice(&bytes)?;
+    Ok(data)
 }
--- common/src/crypto/symmetric.rs
+++ common/src/crypto/symmetric.rs
@@ -2,69 +2,55 @@ use crate::crypto::common::EncryptedValue;
 use aes::cipher::generic_array::GenericArray;
 use aes::cipher::{BlockDecrypt, BlockEncrypt, BlockSizeUser};
 use aes::Aes256;
+use anyhow::format_err;
 use argon2::{self, hash_raw, verify_raw, Config};
 use crypto_box::aead::KeyInit;
-use std::error::Error;
 
 use base64::{engine::general_purpose::URL_SAFE, Engine};
 use rand_core::{OsRng, RngCore};
-pub fn hash(message: &str, salt: &str) -> Result<EncryptedValue, Box<dyn Error>> {
+pub fn hash(message: &str, salt: &str) -> anyhow::Result<EncryptedValue> {
     let config = Config::default();
-    let salt = match URL_SAFE.decode(salt) {
-        Ok(salt) => salt,
-        Err(e) => return Err(Box::new(e)),
-    };
+    let salt = URL_SAFE.decode(salt)?;
 
-    match hash_raw(message.as_bytes(), salt.as_slice(), &config) {
-        Ok(hash) => Ok(EncryptedValue {
-            cipher: URL_SAFE.encode(hash),
-            nonce: URL_SAFE.encode(salt),
-        }),
-        Err(e) => Err(Box::new(e)),
-    }
+    let hash = hash_raw(message.as_bytes(), salt.as_slice(), &config)?;
+    Ok(EncryptedValue {
+        cipher: URL_SAFE.encode(hash),
+        nonce: URL_SAFE.encode(salt),
+    })
 }
 
-pub fn verify_password(password: &str, hash: &str, salt: &str) -> Result<(), Box<dyn Error>> {
-    let hash = match URL_SAFE.decode(hash) {
-        Ok(hash) => hash,
-        Err(e) => return Err(Box::new(e)),
-    };
+pub fn verify_password(password: &str, hash: &str, salt: &str) -> anyhow::Result<()> {
+    let hash = URL_SAFE.decode(hash)?;
 
-    let salt = match URL_SAFE.decode(salt) {
-        Ok(salt) => salt,
-        Err(e) => return Err(Box::new(e)),
-    };
+    let salt = URL_SAFE.decode(salt)?;
 
     let config = Config::default();
-    match verify_raw(
+    let valid = verify_raw(
         password.as_bytes(),
         salt.as_slice(),
         hash.as_slice(),
         &config,
-    ) {
-        Ok(valid) => {
-            if valid {
-                Ok(())
-            } else {
-                Err("Incorrect password".into())
-            }
-        }
-        Err(e) => Err(Box::new(e)),
+    )?;
+
+    if valid {
+        Ok(())
+    } else {
+        Err(format_err!("Invalid password"))
     }
 }
 
 const SALT_BYTES: usize = 16;
-pub fn generate_salt() -> Result<String, Box<dyn Error>> {
+pub fn generate_salt() -> anyhow::Result<String> {
     let mut salt: [u8; SALT_BYTES] = [0; SALT_BYTES];
     OsRng.fill_bytes(&mut salt);
     Ok(URL_SAFE.encode(salt.as_slice()))
 }
 
-pub fn encrypt_data(key: &str, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+pub fn encrypt_data(key: &str, data: &[u8]) -> anyhow::Result<Vec<u8>> {
     let key = URL_SAFE.decode(key)?;
     let aes = match Aes256::new_from_slice(key.as_slice()) {
         Ok(aes) => aes,
-        Err(_) => return Err("Invalid key".into()),
+        Err(_) => return Err(format_err!("Invalid key")),
     };
 
     let block_size = Aes256::block_size();
@@ -83,11 +69,11 @@ pub fn encrypt_data(key: &str, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
     Ok(enc)
 }
 
-pub fn decrypt_data(key: &str, enc: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
+pub fn decrypt_data(key: &str, enc: Vec<u8>) -> anyhow::Result<Vec<u8>> {
     let key = URL_SAFE.decode(key)?;
     let aes = match Aes256::new_from_slice(key.as_slice()) {
         Ok(aes) => aes,
-        Err(_) => return Err("Invalid key".into()),
+        Err(_) => return Err(format_err!("Invalid key")),
     };
 
     let block_size = Aes256::block_size();
